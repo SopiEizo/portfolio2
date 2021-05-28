49901959751962ae3aa86e5bf06e5ac96eeacef9
sopi <sopisaneizo@gmail.com>
2021-05-29 02:02:32 +0900

    アニメーションつけた

---
+++ css/huwari.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+jQuery(function(){
+jQuery('.animation').css('visibility','hidden');
+jQuery(window).scroll(function(){
+ var windowHeight = jQuery(window).height(),
+     topWindow = jQuery(window).scrollTop();
+ jQuery('.animation').each(function(){
+  var targetPosition = jQuery(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   jQuery(this).addClass("fadeInDown");
+  }
+ });
+});
+});
