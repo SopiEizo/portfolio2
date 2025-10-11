0bca47f41f72e262cd47e14798b6cd4fdedc72de
SopiEizo <76783566+SopiEizo@users.noreply.github.com>
2025-10-11 20:27:20 +0900

    [update]serverに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  output: 'hybrid',
+  output: 'server',
   adapter: netlify(),
 });
